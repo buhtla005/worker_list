b241aae4f1b044faf3cd4b4d7f7cdbb00f982579
unknown <noobprogramer@yahoo.com>
2020-03-03 18:56:19 +0100

    Updates

---
+++ precache-manifest.b7d2776189542524bffbd67f096f3d6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e9bc7c0c7904407bb99d3df313cea8",
+    "url": "/worker_list/index.html"
+  },
+  {
+    "revision": "a5e04fae99fa0b0faf6d",
+    "url": "/worker_list/static/css/main.3b42cc48.chunk.css"
+  },
+  {
+    "revision": "23f65b89284c4819b2ca",
+    "url": "/worker_list/static/js/2.b9e0f3ef.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/worker_list/static/js/2.b9e0f3ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e04fae99fa0b0faf6d",
+    "url": "/worker_list/static/js/main.3b8e6ef2.chunk.js"
+  },
+  {
+    "revision": "f7ce1ca080fec9d6b22d",
+    "url": "/worker_list/static/js/runtime-main.83100499.js"
+  }
+]);
